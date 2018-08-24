5573d34c6086cfa72c22ef4439b9edaa5ccb65f4
viney20 <viney1962.cse16@chitkara.edu.in>
2018-08-25 01:52:21 +0530

    code

---
+++ assignment8.py
@@ -0,0 +1,120 @@
+#Q.1- Create a circle class and initialize it with radius. Make two methods getArea and getCircumference inside this class.
+class circle:
+    r = int(input("Enter radius"))
+    def getArea(self):
+        print("Area of circe", 3.14 * self.r * self.r)
+    def getCircumference(self):
+        print("Circumference of circe", 2 * 3.14 * self.r)
+x = circle()
+x.getArea()
+x.getCircumference()
+#Create a Student class and initialize it with name and roll number
+class student:
+    def __init__(self):
+        self.name = input("Enter name")
+        self.rollno = int(input("Enter roll no"))
+
+    def setAge(self):
+        self.age = int(input("Enter age"))
+
+    def setMarks(self):
+        self.marks = int(input("Enter marks"))
+
+    def display(self):
+        print("name =", self.name, " rollno =", self.rollno, " age =", self.age, " marks =", self.marks)
+
+
+x = student()
+x.setAge()
+x.setMarks()
+x.display()
+
+
+#Q.3 Create a Temprature class and create two methods
+class temperature:
+    def convertFarenheit(self, c):
+        print("farenheit = ", c * 1.8 + 32)
+
+    def convertCelsius(self, f):
+        print("celsius = ", (f - 32) / 1.8)
+
+
+c = float(input("enter celcius value"))
+t = temperature()
+t.convertFarenheit(c)
+f = (input("enter farenheit value"))
+t.convertCelsius(f)
+"""
+#Q.4- Create a class MovieDetails and initialize it with artistname,Year of release and ratings .
+class MovieDetails:
+    def __init__(self):
+        self.name = input("Enter aritist_name")
+        self.year = int(input("Enter Year of release"))
+        self.rating = int(input("Enter Ratings"))
+
+    def display(self):
+        print("Artist name =", self.name, " Year of release=", self.year," Rating =", self.rating )
+x=MovieDetails()
+x.display()
+    
+
+#Q.5 Create a class Animal as a base class and define method animal_attribute. Create another class Tiger which is inheriting Animal and access the base class method.
+class Animal:
+    def animal_attribute(self):
+        print("animal")
+class Tiger(Animal):
+    def tiger(self):
+        print("tiger")
+
+a = Animal()
+t = Tiger()
+t.animal_attribute()
+
+
+
+#Q.6- What will be the output of following code.
+"""
+ANS:-> #geting error because interprator wants '(' this  in line no 14&15  AFTER  solving error output is
+'A', 'B'
+'A', 'B'
+
+
+
+
+"""
+#Q.7 Create a class Shape.Initialize it with length and breadth Create the method Area. Create class rectangle and square which inherits shape and access the method Area.
+class shape:
+    def __init__(self,length,breath):
+        self.length=length
+        self.breath=breath
+        
+class square(shape):
+    def __init__(self, shape):
+        self.length= shape.length
+        self.breath= shape.breath
+        self.area
+    def area(self):
+        area = self.length * self.breath
+        print("square area= ", area)
+
+
+class rectangle(shape):
+    def __init__(self, shape):
+        self.length= shape.length
+        self.breath= shape.breath
+        self.area
+    def area(self):
+        area = self.length * self.breath
+        print("rectangle area= ", area)
+
+length = int(input("enter length"))
+breadth = int(input("enter breadth"))
+s = shape(length,breadth)
+sq = square(s)
+re = rectangle(s)
+if length==breadth:
+    sq.area()
+else:
+    re.area()
+
+
